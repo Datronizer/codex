5883bcad71f31ae9240c04873acc15a427dc4bae
Datronizer <tqc1234@gmail.com>
2023-04-05 18:58:27 +0700

    Update HexViewer.tsx
    Added funny little color select thing

--- src/components/hex-viewer/HexViewer.tsx
+++ src/components/hex-viewer/HexViewer.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
+
 import { formatHexColor, setColor } from '../../util';
 
 /**
@@ -42,22 +47,49 @@ class InternalHexViewer extends React.Component<InternalHexViewerProps, Internal
     public render(): React.ReactNode {
         return (
             <Container>
-                <Form onSubmit={e => this.onSubmit(e)}>
-                    <Form.Group className="mb-3" controlId="hexInput">
-                        <Form.Label>Hex Value</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Ex: #ffffff or ffffff"
-                            value={this.state.colorInput}
-                            onChange={e => { this.setState({ colorInput: e.target.value }) }}
-                        />
-                        <Form.Text className="text-muted">
-                            Enter a hex value to view its color
-                        </Form.Text>
-                    </Form.Group>
+                <Row>
+                    <Col>
+                        <h2>Color viewer</h2>
+                        <Form onSubmit={e => this.onSubmit(e)}>
+                            <Form.Group className="mb-3" controlId="hexInput">
+                                <Form.Label>Hex Value</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ex: #ffffff or ffffff"
+                                    value={this.state.colorInput}
+                                    onChange={e => { this.setState({ colorInput: e.target.value }) }}
+                                />
+                                <Form.Text className="text-muted">
+                                    Enter a hex value to view its color
+                                </Form.Text>
+                                <br />
+                                <Button variant="primary" type="submit">Submit</Button>
+                            </Form.Group>
 
-                    <Button variant="primary" type="submit">Submit</Button>
-                </Form>
+                            <Form.Group>
+                                <Form.Label>Color Picker</Form.Label>
+                                <Form.Control
+                                    type='color'
+                                    id='colorPicker'
+                                    defaultValue={this.state.color}
+                                    onChange={e => {
+                                        setColor(e.target.value);
+                                        this.setState({ color: document.body.style.background })
+                                    }}
+                                />
+                                <Form.Text className="text-muted">
+                                    Alternatively, you can use this color picker
+                                </Form.Text>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                    <Col>
+                        <h2>Your current color is </h2>
+                        {this.state.color + "ahhh"}
+
+                    </Col>
+                </Row>
+
             </Container>
         );
     }
