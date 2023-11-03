d5196dbabe1f6dde8e90b32ed9b23ded79b570c0
Datronizer <tqc1234@gmail.com>
2023-11-03 12:19:09 -0400

    Type fixing

--- src/components/app/AppRouter.tsx
+++ src/components/app/AppRouter.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Dashboard from "../dashboard/Dashboard";
 import HexViewer from "../hex-viewer/HexViewer";
 import { ObsidianViewer } from "../obsidian/ObsidianViewer";
+import { Home } from "../home/Home";
 
 export function AppRouter() {
     return (
@@ -10,6 +12,7 @@ export function AppRouter() {
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/home" element={<Home />} />
 
                 <Route path="/hex" element={<HexViewer />} />
                 <Route path="/obsidian" element={<ObsidianViewer />} />
